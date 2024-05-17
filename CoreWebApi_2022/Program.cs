6fb692543129b9d6fd7f5ed15d3cad5cd0e38dc6
yong880323 <yong880323@gmail.com>
2024-05-17 17:46:30 +0800

    #新增Nlog

--- CoreWebApi_2022/Program.cs
+++ CoreWebApi_2022/Program.cs
@@ -1,25 +1,48 @@
-var builder = WebApplication.CreateBuilder(args);
+using NLog;
+using NLog.Web;
 
 // Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+var logger = LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+try
 {
-	app.UseSwagger();
-	app.UseSwaggerUI();
+	logger.Debug("init main");
+	var builder = WebApplication.CreateBuilder(args);
+
+	//將NLog註冊到此專案內
+	builder.Logging.ClearProviders();
+	//設定log紀錄的最小等級
+	//builder.Logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Information);
+	builder.Host.UseNLog();
+	builder.Services.AddControllers();
+	// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+	builder.Services.AddEndpointsApiExplorer();
+	builder.Services.AddSwaggerGen();
+
+	var app = builder.Build();
+
+	// Configure the HTTP request pipeline.
+	if (app.Environment.IsDevelopment())
+	{
+		app.UseSwagger();
+		app.UseSwaggerUI();
+	}
+
+	app.UseHttpsRedirection();
+
+	app.UseAuthorization();
+
+	app.MapControllers();
+
+	app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+	// 捕獲設定錯誤的錯誤紀錄
+	logger.Error(ex, "Stopped program because of exception");
+	throw;
+}
+finally
+{
+	//須確定在關閉時，把nlog關閉
+	LogManager.Shutdown();
+}
